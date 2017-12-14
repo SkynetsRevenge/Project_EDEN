86da25b7e46ecf643b7eba39356a4ffa0f5f8bfe
SkynetsRevenge <34491979+SkynetsRevenge@users.noreply.github.com>
2017-12-13 20:04:06 -0800

    Update TilesMAIN.h

--- TilesMAIN.h
+++ TilesMAIN.h
@@ -1,13 +1,61 @@
-#ifndef MENU2_H_INCLUDED
-#define MENU2_H_INCLUDED
+#ifndef TILES_H_INCLUDED
+#define TILES_H_INCLUDED
 #include <iostream>
-
+//use cordinaites n'shit north+1 to y, south-1 to y, west-1 x, east+1 x
+//spawn (24,26)
+// "We art not but babies, and these babies must walk" - Phillip 2k16
 using namespace std;
-//hii
 
-void menu()
+void playerloction()
 {
-
+  char movement[20];
+	int cordx=24;
+	int cordy=26;
+  //equal cords in tile24_26
+  tile24_26(cordx, cordy);
+  cin << movement;
+  if(movement == 'north')
+  {
+    cordy = cordy + 1
+  }
+      
+  if(movement == 'south')
+  {
+   	cordy = cordy - 1;
+  }
+  
+  if(movement == 'east')
+  {
+   	cordx = cordx + 1; 
+  }
+  
+  if(movement == 'west')
+  {
+   	cordx = cordx - 1; 
+  }
+  
+  
 }
-
-#endif // MENU2_H_INCLUDED
+//normal ass tiles - items/normal boss encounters
+void tile24_26(int &cordx, &cordy)
+{
+	//spawn
+  
+}
+//town tiles
+void tile#()
+{
+	
+}
+//dungeon tiles
+void tile0()
+{
+	
+}
+//radiation tiles
+void tile0()
+{
+	
+}
+#endif // TILES_H_INCLUDED
+//skynet was here
